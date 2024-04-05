76e4618ba3fdd94f94d14e439603265469a411c4
Aimar Karrik <71828910+AimarKarrik@users.noreply.github.com>
2024-04-05 15:01:20 +0300

    Structure Changes. Api base.

---
+++ Api/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import router from './router/router';
+const port: number = 3001;
+
+const app: express.Application = express();
+app.use(express.json());
+app.use(router)
+
+app.listen(port, () => {
+    console.log(`Api is hosted on port: ${port}`);
+});
---
+++ Frontend/src/api/api.ts
@@ -0,0 +1,15 @@
+import { SpotifyApi, AuthorizationCodeWithPKCEStrategy } from '@spotify/web-api-ts-sdk'
+
+const auth = new AuthorizationCodeWithPKCEStrategy(
+    'a8db7a861337459eb17dc0bcaa86f671',
+    'http://localhost:5173',
+    [
+        'user-read-private',
+        'user-read-email',
+        'playlist-modify-public',
+        'playlist-modify-private, user-read-playback-state, user-modify-playback-state'
+    ]
+)
+
+const spotify = new SpotifyApi(auth)
+export default spotify
