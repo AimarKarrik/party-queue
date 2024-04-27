af7e292e816ebe0b14ac600a5476ead4f1ffca3c
Aimar Karrik <71828910+AimarKarrik@users.noreply.github.com>
2024-04-27 16:32:25 +0300

    fixed setup.bat

--- setup.bat
+++ setup.bat
@@ -1,33 +1,29 @@
 @echo off
 
-rem Define directories
-set dir1=./Api
-set dir2=./Client
-
-rem Install npm dependencies for directory 1
-echo Installing npm dependencies for API...
-cd %dir1%
-npm install
+echo Installing npm dependencies for API ...
+cd api
+call npm i
 
 rem Check for npm install error
 if %errorlevel% neq 0 (
-    echo Error: npm install failed for API.
-    pause >nul
+    echo Error: npm install failed for %~1.
+    pause
     exit /b %errorlevel%
 )
-
-rem Install npm dependencies for directory 2
-echo Installing npm dependencies for CLIENT...
 cd ..
-cd %dir2%
-npm install
+
+echo Installing npm dependencies for client ...
+cd client
+call npm i
 
 rem Check for npm install error
 if %errorlevel% neq 0 (
-    echo Error: npm install failed for CLIENT.
-    pause >nul
+    echo Error: npm install failed for %~1.
+    pause
     exit /b %errorlevel%
 )
+cd ..
+
 
 echo npm install completed successfully for both directories.
-pause >nul
+pause
